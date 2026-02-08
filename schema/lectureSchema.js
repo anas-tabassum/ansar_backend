7b4d6d38caa7630b730953148ec0212dfb55080b
boubacaride <39181066+boubacaride@users.noreply.github.com>
2026-02-08 04:11:42 -0500

    Add 'year' field to LectureSchema for lesson sorting by year
    Added 'year' field to LectureSchema.

--- schema/lectureSchema.js
+++ schema/lectureSchema.js
@@ -1,23 +1,28 @@
 const mongoose = require('mongoose');
 
 const LectureSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    url: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    }
+        title: {
+                    type: String,
+                    required: true,
+                    trim: true
+        },
+        url: {
+                    type: String,
+                    required: true,
+                    trim: true
+        },
+        description: {
+                    type: String,
+                    required: true,
+                    trim: true
+        },
+        year: {
+                    type: String,
+                    required: true,
+                    trim: true
+        }
 }, {
-    timestamps: true
+        timestamps: true
 });
 
 const lecture = mongoose.model('lectures', LectureSchema);
